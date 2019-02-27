344be5d71fc10fdc49deb8950c6b11bbdbba3110
nrista-org <narista.sa@gmail.com>
2019-02-27 20:14:33 +0700

    min

--- src/main/java/com/demo/captcha/Captcha.java
+++ src/main/java/com/demo/captcha/Captcha.java
@@ -1,8 +1,10 @@
 package com.demo.captcha;
 
 public class Captcha {
-    private  int pattern;
+    private int pattern;
     private int leftOperand;
+    private int operator;
+    private int rightOperand;
 
     public int getPattern() {
         return pattern;
@@ -36,10 +38,8 @@ public class Captcha {
         this.rightOperand = rightOperand;
     }
 
-    private  int operator;
-    private int rightOperand;
 
-    public Captcha( int pattern, int leftOperand, int operator, int rightOperand) {
+    public Captcha(int pattern, int leftOperand, int operator, int rightOperand) {
         this.pattern = pattern;
         this.leftOperand = leftOperand;
         this.operator = operator;
@@ -47,7 +47,30 @@ public class Captcha {
     }
 
     @Override
-    public String toString(){
-        return "1 + One";
+    public String toString() {
+
+        return String.format("%s %s %s",leftOperand,"+","One");
+        /*switch (this.getLeftOperand()) {
+            case 1:
+                return "1 + One";
+
+            case 2:
+                return "2 + One";
+
+            case 3:
+                return "3 + One";
+
+            case 4:
+                return "4 + One";
+
+            case 5:
+                return "5 + One";
+
+            default:
+                return "1";
+
+        }*/
+
+
     }
 }
--- src/test/java/com/demo/captcha/CaptchaTest.java
+++ src/test/java/com/demo/captcha/CaptchaTest.java
@@ -10,6 +10,31 @@ public class CaptchaTest {
     public void firstPatternLeftOperandShouldBe1(){
         Captcha captcha = new Captcha(1,1,1,1);
         Assert.assertEquals("1 + One",captcha.toString());
+
     }
 
+    @Test
+    public void firstPatternLeftOperandShouldBe2(){
+        Captcha captcha = new Captcha(1,2,1,1);
+        Assert.assertEquals("2 + One",captcha.toString());
+
+    }
+    @Test
+    public void firstPatternLeftOperandShouldBe3(){
+        Captcha captcha = new Captcha(1,3,1,1);
+        Assert.assertEquals("3 + One",captcha.toString());
+
+    }
+    @Test
+    public void firstPatternLeftOperandShouldBe4(){
+        Captcha captcha = new Captcha(1,4,1,1);
+        Assert.assertEquals("4 + One",captcha.toString());
+
+    }
+    @Test
+    public void firstPatternLeftOperandShouldBe5(){
+        Captcha captcha = new Captcha(1,5,1,1);
+        Assert.assertEquals("5 + One",captcha.toString());
+
+    }
 }
