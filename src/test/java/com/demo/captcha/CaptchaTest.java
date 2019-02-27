83c6954b3fcd84e48739a1bee87b93ab2e61d39e
nrista-org <narista.sa@gmail.com>
2019-02-27 19:54:58 +0700

    min

--- src/test/java/com/demo/captcha/CaptchaTest.java
+++ src/test/java/com/demo/captcha/CaptchaTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class CaptchaTest {
     @Test
     public void firstPatternLeftOperandShouldBe1(){
-        Captcha captcha = new Captcha();
+        Captcha captcha = new Captcha(1,1,1,1);
         Assert.assertEquals("1 + One",captcha.toString());
     }
 
